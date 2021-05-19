adf118bd6be27c2c4fee6cf3ab667918a0f59bfb
DinhHuynhTienPhu <DinhHuynhTienPhu@users.noreply.github.com>
2021-05-19 18:38:36 +0700

    create ASP-NET-MVC folder, find out template, try to use some method

---
+++ ASP-NET-MVC/WebApplication1/WebApplication1/Controllers/EditorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EditorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit() {
+            ViewBag.Message = "Edit page";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "login page";
+
+            return View("Edit");
+        }
+        [HttpPost]
+        public ActionResult PostName(string personName) {
+            ViewBag.name = personName;
+            return View("PostName");
+        }
+    }
+}
