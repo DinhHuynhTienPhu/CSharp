68ba3ea9232d730eb75eb39a60cae788cd964a14
DinhHuynhTienPhu <DinhHuynhTienPhu@users.noreply.github.com>
2021-05-23 22:30:39 +0700

    update 3.2 (work with STUDENT table) and a part of 3.3

--- ASP-NET-MVC/WebApplication1/DatabaseIO/DBIO.cs
+++ ASP-NET-MVC/WebApplication1/DatabaseIO/DBIO.cs
@@ -20,7 +20,7 @@ namespace DatabaseIO
             student.ID = id;
             student.STU_NAME = name;
             DateTime dt;
-            DateTime.TryParseExact(date, "yyyyMMdd",
+            DateTime.TryParseExact(date, "yyyy/MM/dd",
                           CultureInfo.InvariantCulture,
                           DateTimeStyles.None, out dt);
 
@@ -33,10 +33,73 @@ namespace DatabaseIO
         public List<Student> GetAllStudent() {
             return myDB.Database.SqlQuery<Student>("select* from STUDENT").ToList();
         }
-
         public void DeleteStudent(string id,string school) {
             myDB.Database.ExecuteSqlCommand("DELETE FROM STUDENT WHERE id='"+id+"'AND UNI_ID='"+school+"'");
 
         }
+        public void EditStudent(string id,string name, string date, string cclass, string school, string department) {
+            //myDB.Database.ExecuteSqlCommand("insert into Student values ('" + id + "',N'" + name + "','" + date + "','" + cclass + "','" + school + "','" + department + "')");
+
+            myDB.Database.ExecuteSqlCommand("update Student set STU_NAME  = '"+name+"' where Student.ID = '" + id+"'");
+
+            myDB.Database.ExecuteSqlCommand("update Student set DATE_OF_BIRTH   = '" + date+"' where Student.ID = '" + id+"'");
+            myDB.Database.ExecuteSqlCommand("update Student set CLA_ID   = '" + cclass+"' where Student.ID = '" + id+"'");
+            myDB.Database.ExecuteSqlCommand("update Student set UNI_ID   = '" + school+"' where Student.ID = '" + id+"'");
+            myDB.Database.ExecuteSqlCommand("update Student set DEP_ID   = '" + department+"' where Student.ID = '" + id+"'");
+        }
+        #region class
+        public Class GetClassByID(string id) {
+            string SQL = "select * from class where ID='" + id + "'";
+           return myDB.Database.SqlQuery<Class>(SQL).FirstOrDefault();
+        }
+        public void AddClass(string id, string name,  string school, string department) {
+            string sql = "insert into Class(ID,CLA_NAME,UNI_ID,DEP_ID) values ('" + id + " ',N'" + name + " ','" + school + " ','" + department + "')";
+            myDB.Database.ExecuteSqlCommand(sql);
+        }
+        public List<Class> GetAllClass() {
+            return myDB.Database.SqlQuery<Class>("select* from CLASS").ToList();
+        }
+        public void DeleteClass(string id,string department,string school) {
+            myDB.Database.ExecuteSqlCommand("DELETE FROM CLASS WHERE id='"+id+"'AND UNI_ID='"+school+"' and DEP_ID='"+department+"'" );
+
+        }
+        public void EditClass(string id,string name, string school, string department) {
+            //myDB.Database.ExecuteSqlCommand("insert into Student values ('" + id + "',N'" + name + "','" + date + "','" + cclass + "','" + school + "','" + department + "')");
+
+            myDB.Database.ExecuteSqlCommand("update Class set CLA_NAME  = '"+name+"' where ID = '" + id+"'");
+
+            myDB.Database.ExecuteSqlCommand("update Class set UNI_ID   = '" + school+"' where ID = '" + id+"'");
+            myDB.Database.ExecuteSqlCommand("update Class set DEP_ID   = '" + department+"' where ID = '" + id+"'");
+        }
+        #endregion
+
+        #region department
+        public Department GetDepartmentByID(string id,string school)
+        {
+            string SQL = "select * from Department where ID='" + id + "' and UNI_ID='"+school+"'";
+            return myDB.Database.SqlQuery<Department>(SQL).FirstOrDefault();
+        }
+        public void AddDepartment(string id, string name, string school)
+        {
+            string sql = "insert into department(ID,DEP_NAME,UNI_ID) values ('" + id + " ',N'" + name + " ','" + school + "')";
+            myDB.Database.ExecuteSqlCommand(sql);
+        }
+        public List<Department> GetAllDepartment()
+        {
+            return myDB.Database.SqlQuery<Department>("select* from department").ToList();
+        }
+        public void DeleteDepartment(string id,  string school)
+        {
+            myDB.Database.ExecuteSqlCommand("DELETE FROM department WHERE id='" + id + "'AND UNI_ID='" + school + "' ");
+
+        }
+        public void EditDepartment(string id, string name, string school)
+        {
+            //myDB.Database.ExecuteSqlCommand("insert into Student values ('" + id + "',N'" + name + "','" + date + "','" + cclass + "','" + school + "','" + department + "')");
+
+            myDB.Database.ExecuteSqlCommand("update department set DEP_NAME  = '" + name + "' where ID = '" + id + "' and UNI_ID='"+school+"'");
+
+        }
+        #endregion
     }
 }
--- ASP-NET-MVC/WebApplication1/WebApplication1/Controllers/EditorController.cs
+++ ASP-NET-MVC/WebApplication1/WebApplication1/Controllers/EditorController.cs
@@ -29,6 +29,7 @@ namespace WebApplication1.Controllers
         public ActionResult QueryPage() {
             return View();
         }
+        #region student
         public ActionResult ReturnStudentByID( string studentID) {
             DBIO dbio = new DBIO();
             Student stu = dbio.GetStudentByID(studentID);
@@ -53,7 +54,98 @@ namespace WebApplication1.Controllers
            return View("ReturnAllStudent",dbio.GetAllStudent());
             
         }
+        public ActionResult EditStudent(string studentID) {
+            DBIO dbio = new DBIO();
+            Student stu = dbio.GetStudentByID(studentID);
+            return View(stu);
+        }
+        public ActionResult DoneEditStudent(string id, string name, string date, string cclass, string school, string department) {
 
+
+            DBIO dbio = new DBIO();
+            dbio.EditStudent(id, name, date, cclass, school, department);
+            ViewBag.notification = "Updated your changes";
+            return View("Notify");
+        }
+        #endregion
+        #region class
+        public ActionResult ReturnClassByID( string classID) {
+            DBIO dbio = new DBIO();
+            Class cclass = dbio.GetClassByID(classID);
+            return View(cclass);
+        }
+        public ActionResult AddClass(string id, string name,  string school, string department) {
+            DBIO dbio = new DBIO();
+
+            dbio.AddClass(id, name,  school, department);
+
+            ViewBag.notification = "Added class with id= " + id;
+            return View("Notify");
+        }
+        public ActionResult DeleteClass(string classID,string school, string department) {
+            DBIO dbio = new DBIO();
+            dbio.DeleteClass(classID, department, school);
+            ViewBag.notification = "removed class with id= " + classID;
+            return View("Notify");
+        }
+        public ActionResult GetAllClass() {
+            DBIO dbio = new DBIO();
+           return View("ReturnAllClass",dbio.GetAllClass());
+            
+        }
+        public ActionResult EditClass(string classID) {
+            DBIO dbio = new DBIO();
+            Class c = dbio.GetClassByID(classID);
+            return View(c);
+        }
+        public ActionResult DoneEditClass(string id, string name, string school, string department) {
+
+
+            DBIO dbio = new DBIO();
+            dbio.EditClass(id, name, school, department);
+            ViewBag.notification = "Updated your changes";
+            return View("Notify");
+        }
+        #endregion 
+        #region department
+        public ActionResult ReturnDepartmentByID( string departmentID,string school) {
+            DBIO dbio = new DBIO();
+            Department d = dbio.GetDepartmentByID(departmentID,school);
+            return View(d);
+        }
+        public ActionResult AddDepartment(string id, string name,  string school) {
+            DBIO dbio = new DBIO();
+
+            dbio.AddDepartment(id, name,  school);
+
+            ViewBag.notification = "Added department with id= " + id;
+            return View("Notify");
+        }
+        public ActionResult DeleteDepartment(string school, string department) {
+            DBIO dbio = new DBIO();
+            dbio.DeleteDepartment(department, school);
+            ViewBag.notification = "removed depatment with id= " + department;
+            return View("Notify");
+        }
+        public ActionResult GetAllDepartment() {
+            DBIO dbio = new DBIO();
+           return View("ReturnAllDepartment",dbio.GetAllDepartment());
+            
+        }
+        public ActionResult EditDepartment(string departmentID,string school) {
+            DBIO dbio = new DBIO();
+            Department  c = dbio.GetDepartmentByID(departmentID,school);
+            return View(c);
+        }
+        public ActionResult DoneEditDepartment(string id, string name, string school) {
+
+
+            DBIO dbio = new DBIO();
+            dbio.EditDepartment(id, name,  school);
+            ViewBag.notification = "Updated your changes";
+            return View("Notify");
+        }
+        #endregion
         public ActionResult Notify() {
 
             return View();
