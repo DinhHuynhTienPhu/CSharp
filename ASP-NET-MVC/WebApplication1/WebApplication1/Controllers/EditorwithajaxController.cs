34bc72c6283dfa30a3d7f5c9dd1477b0c3a71062
DinhHuynhTienPhu <DinhHuynhTienPhu@users.noreply.github.com>
2021-06-18 22:02:40 +0700

    4.2 Student edit with ajax

--- ASP-NET-MVC/WebApplication1/WebApplication1/Controllers/EditorwithajaxController.cs
+++ ASP-NET-MVC/WebApplication1/WebApplication1/Controllers/EditorwithajaxController.cs
@@ -2,106 +2,134 @@
 using DatabaseProvider;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
-    public class EditorController : Controller
+    public class EditorwithajaxController : Controller
     {
+        // GET: EditorWithAjax
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Edit() {
-            ViewBag.Message = "Edit page";
-
-            return View();
-        }
-        public ActionResult Login()
+        public ActionResult QueryPageWithAjax()
         {
-            ViewBag.Message = "login page";
-
-            return View("Edit");
-        }
-        public ActionResult QueryPage() {
-            return View();
-        }
-        public ActionResult QueryPageWithAjax() {
-            return View();
+            return View("QueryPageWithAjax");
         }
         #region student
-        public ActionResult ReturnStudentByID( string studentID) {
+        [HttpGet]
+        public ActionResult ReturnStudentByID(string studentID)
+        {
             DBIO dbio = new DBIO();
             Student stu = dbio.GetStudentByID(studentID);
+            var returnStudent = new Student() {
+                STU_NAME = stu.STU_NAME.ToString(),
+                DATE_OF_BIRTH = stu.DATE_OF_BIRTH,
+                CLA_ID = stu.CLA_ID.ToString(),
+                DEP_ID = stu.DEP_ID.ToString(),
+                UNI_ID = stu.UNI_ID.ToString()
+
+            };
+            return Json(returnStudent, JsonRequestBehavior.AllowGet);
             return View(stu);
         }
-        public ActionResult AddStudent(string id, string name, string date, string cclass, string school, string department) {
+        [HttpPost]
+        public ActionResult AddStudent(string id, string name, string date, string cclass, string school, string department)
+        {
             DBIO dbio = new DBIO();
 
             dbio.AddStudent(id, name, date, cclass, school, department);
 
             ViewBag.notification = "Added student with id= " + id;
+            return Json(new object());
             return View("Notify");
         }
-        public ActionResult DeleteStudent(string studentID,string school) {
+                [HttpPost]
+        public ActionResult DeleteStudent(string studentID, string school)
+        {
             DBIO dbio = new DBIO();
-            dbio.DeleteStudent(studentID,school);
+            dbio.DeleteStudent(studentID, school);
             ViewBag.notification = "removed student with id= " + studentID;
+            return Json(new object());
+
             return View("Notify");
         }
-        public ActionResult GetAllStudent() {
+        public ActionResult GetAllStudent()
+        {
             DBIO dbio = new DBIO();
-           return View("ReturnAllStudent",dbio.GetAllStudent());
-            
+            return Json(dbio.GetAllStudent(), JsonRequestBehavior.AllowGet);
+            return View("ReturnAllStudent", dbio.GetAllStudent());
+
         }
-        public ActionResult EditStudent(string studentID) {
+        [HttpGet]
+        public ActionResult EditStudent(string studentID)
+        {
             DBIO dbio = new DBIO();
             Student stu = dbio.GetStudentByID(studentID);
+            var returnStudent = new Student()
+            {
+                STU_NAME = stu.STU_NAME.ToString(),
+                DATE_OF_BIRTH = stu.DATE_OF_BIRTH,
+                CLA_ID = stu.CLA_ID.ToString(),
+                DEP_ID = stu.DEP_ID.ToString(),
+                UNI_ID = stu.UNI_ID.ToString()
+
+            };
+            return Json(returnStudent, JsonRequestBehavior.AllowGet);
             return View(stu);
         }
-        public ActionResult DoneEditStudent(string id, string name, string date, string cclass, string school, string department) {
+        [HttpPost]
+        public ActionResult DoneEditStudent(string id, string name, string date, string cclass, string school, string department)
+        {
 
 
             DBIO dbio = new DBIO();
             dbio.EditStudent(id, name, date, cclass, school, department);
             ViewBag.notification = "Updated your changes";
+            return Json(id+" "+ name);
             return View("Notify");
         }
         #endregion
         #region class
-        public ActionResult ReturnClassByID( string classID) {
+        public ActionResult ReturnClassByID(string classID)
+        {
             DBIO dbio = new DBIO();
             Class cclass = dbio.GetClassByID(classID);
             return View(cclass);
         }
-        public ActionResult AddClass(string id, string name,  string school, string department) {
+        public ActionResult AddClass(string id, string name, string school, string department)
+        {
             DBIO dbio = new DBIO();
 
-            dbio.AddClass(id, name,  school, department);
+            dbio.AddClass(id, name, school, department);
 
             ViewBag.notification = "Added class with id= " + id;
             return View("Notify");
         }
-        public ActionResult DeleteClass(string classID,string school, string department) {
+        public ActionResult DeleteClass(string classID, string school, string department)
+        {
             DBIO dbio = new DBIO();
             dbio.DeleteClass(classID);
             ViewBag.notification = "removed class with id= " + classID;
             return View("Notify");
         }
-        public ActionResult GetAllClass() {
+        public ActionResult GetAllClass()
+        {
             DBIO dbio = new DBIO();
-           return View("ReturnAllClass",dbio.GetAllClass());
-            
+            return View("ReturnAllClass", dbio.GetAllClass());
+
         }
-        public ActionResult EditClass(string classID,string school,string department) {
+        public ActionResult EditClass(string classID, string school, string department)
+        {
             DBIO dbio = new DBIO();
             Class c = dbio.GetClassByID(classID);
             return View(c);
         }
-        public ActionResult DoneEditClass(string id, string name, string school, string department) {
+        public ActionResult DoneEditClass(string id, string name, string school, string department)
+        {
 
 
             DBIO dbio = new DBIO();
@@ -111,46 +139,52 @@ namespace WebApplication1.Controllers
         }
         #endregion 
         #region department
-        public ActionResult ReturnDepartmentByID( string departmentID) {
+        public ActionResult ReturnDepartmentByID(string departmentID)
+        {
             DBIO dbio = new DBIO();
             Department d = dbio.GetDepartmentByID(departmentID);
             return View(d);
         }
-        public ActionResult AddDepartment(string id, string name,  string school) {
+        public ActionResult AddDepartment(string id, string name, string school)
+        {
             DBIO dbio = new DBIO();
 
-            dbio.AddDepartment(id, name,  school);
+            dbio.AddDepartment(id, name, school);
 
             ViewBag.notification = "Added department with id= " + id;
             return View("Notify");
         }
-        public ActionResult DeleteDepartment(string school, string department) {
+        public ActionResult DeleteDepartment(string school, string department)
+        {
             DBIO dbio = new DBIO();
             dbio.DeleteDepartment(department);
             ViewBag.notification = "removed depatment with id= " + department;
             return View("Notify");
         }
-        public ActionResult GetAllDepartment() {
+        public ActionResult GetAllDepartment()
+        {
             DBIO dbio = new DBIO();
-           return View("ReturnAllDepartment",dbio.GetAllDepartment());
-            
+            return View("ReturnAllDepartment", dbio.GetAllDepartment());
+
         }
-        public ActionResult EditDepartment(string departmentID,string school) {
+        public ActionResult EditDepartment(string departmentID, string school)
+        {
             DBIO dbio = new DBIO();
-            Department  c = dbio.GetDepartmentByID(departmentID);
+            Department c = dbio.GetDepartmentByID(departmentID);
             return View(c);
         }
-        public ActionResult DoneEditDepartment(string id, string name, string school) {
+        public ActionResult DoneEditDepartment(string id, string name, string school)
+        {
 
 
             DBIO dbio = new DBIO();
-            dbio.EditDepartment(id, name,  school);
+            dbio.EditDepartment(id, name, school);
             ViewBag.notification = "Updated your changes";
             return View("Notify");
         }
         #endregion
         #region university/school
-        public ActionResult ReturnUniversityByID( string universityID)
+        public ActionResult ReturnUniversityByID(string universityID)
         {
             DBIO dbio = new DBIO();
             University d = dbio.GetUniversityByID(universityID);
@@ -194,15 +228,5 @@ namespace WebApplication1.Controllers
             return View("Notify");
         }
         #endregion
-        public ActionResult Notify() {
-
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult PostName(string personName) {
-            ViewBag.name = personName;
-            return View("PostName");
-        }
     }
 }
